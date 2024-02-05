fe71191faa7906976a0d45a9977507ada28e80fc
srw.ing <89680829+Ingggggg@users.noreply.github.com>
2024-02-05 21:31:14 +0700

    Lab7.2

--- Employee.java
+++ Employee.java
@@ -1,35 +1,30 @@
-// Component
-public class Employee implements EmployeeComponent {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
     private String name;
     private String position;
     private int salary;
+    private List<Employee> subordinates;
     
     public Employee(String name, String position, int salary) {
         this.name = name;
         this.position = position;
         this.salary = salary;
+        this.subordinates = new ArrayList<>();
     }
-    public String getName() {
-        return name;
+    public void add(Employee e) {
+        subordinates.add(e);
     }
-    public void setName(String name) {
-        this.name = name;
+    public void remove(Employee e) {
+        subordinates.remove(e);
     }
-    public String getPosition() {
-        return position;
-    }
-    public void setPosition(String dept) {
-        this.position = dept;
-    }
-    public int getSalary() {
-        return salary;
-    }
-    public void setSalary(int salary) {
-        this.salary = salary;
+    public List<Employee> getSubordinates() {
+        return subordinates;
     }
 
     @Override
-    public void print() {
-        System.out.println("Employee :[ Name : " + name + ", position : " + position + ", salary : " + salary + " ]");
+    public String toString() {
+        return ("Employee :[ Name : " + name + ", position : " + position + ", salary : " + salary + " ]");
     }
 }
