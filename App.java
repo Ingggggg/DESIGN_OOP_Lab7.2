9d0e8beae2cf36b301fb6650bd3739b72c9c752c
srw.ing <89680829+Ingggggg@users.noreply.github.com>
2024-02-05 21:29:26 +0700

    Update App.java

--- App.java
+++ App.java
@@ -1,26 +1,33 @@
 public class App {
     public static void main(String[] args) {
-        Manager ceo = new Manager("Settha", "CEO", 500000);
-        Manager headSales = new Manager("Kamphaka", "Head Sales", 300000);
+        Employee ceo = new Employee("Settha", "CEO", 500000);
+
+        Employee headSales = new Employee("Kamphaka", "Head Sales", 300000);
         Employee sales1 = new Employee("Wiroj", "Sales", 150000);
         Employee sales2 = new Employee("Weeranan", "Sales", 100000);
 
-        Manager headMarketing = new Manager("UngInk", "Head Marketing", 300000);
+        Employee headMarketing = new Employee("UngInk", "Head Marketing", 300000);
         Employee marketing1 = new Employee("Oak", "Marketing", 200000);
         Employee marketing2 = new Employee("Aem", "Marketing", 250000);
 
-        headSales.addSubordinate(sales1);
-        headSales.addSubordinate(sales2);
+        ceo.add(headSales);
+        headSales.add(sales1);
+        headSales.add(sales2);
 
-        headMarketing.addSubordinate(marketing1);
-        headMarketing.addSubordinate(marketing2);
+        ceo.add(headMarketing);
+        headMarketing.add(marketing1);
+        headMarketing.add(marketing2);
 
-        ceo.addSubordinate(headSales);
-        ceo.addSubordinate(headMarketing);
-
-        printAllEmployee(ceo);
+        printAllEmployee(ceo, 0);
     }
-    public static void printAllEmployee(EmployeeComponent employee) {
-        employee.print();
+
+    public static void printAllEmployee(Employee employee, int level) {
+        String indentation = "";
+        for (int i = 0; i < level; i++) {
+            indentation += "   ";
+        }
+        System.out.println(indentation + employee);
+        for (Employee subordinate : employee.getSubordinates()) 
+            printAllEmployee(subordinate, level + 1);
     }
 }
